e5f154a5aab404bac3c0dd951b45f45ae7fd2acc
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-03-19 18:27:19 -0400

    Add login,signup, and account pages

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SignUp = () => {
+  return (
+    <>
+      <div className="w-full h-screen ">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/d049a3bd-40ee-411b-9f16-d1def798d43b/8ca1907b-c227-4d14-901b-a6114dbc6548/CA-en-20230313-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+          alt="sign-up-image"
+          className="hidden sm:block absolute w-full h-full object-cover"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+        <div className="fixed w-full px-4 py-24 z-50">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-bold">Sign Up</h1>
+              <form className="w-full flex flex-col py-4">
+                <input
+                  className="p-3 my-2 bg-gray-600 rounded"
+                  type="email"
+                  placeholder="Email"
+                  autoComplete="email"
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                  className="p-3 my-2 bg-gray-600 rounded"
+                />
+
+                <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                  Sign Up
+                </button>
+                <div className="flex justify-between items-center text-sm text-gray-600">
+                  <p>
+                    <input type="checkbox" className="mr-2" />
+                    Remember me
+                  </p>
+                  <p>Need help?</p>
+                </div>
+                <p className="py-8">
+                  <span className="text-gray-600">
+                    Already subscribed to Netflix?
+                  </span>{" "}
+                  <Link to={"/login"}>Sign In</Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      ;
+    </>
+  );
+};
+
+export default SignUp;
